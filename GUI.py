1218d53c33901e53a6131e840f4b22847ebc1879
Corpesy <cartmansmad@gmail.com>
2024-02-14 02:22:15 -0800

    Dev State
    Added sorting for programing files.
    
    Now sorts files
    
    Working on GUI
    
    GUI Coming along still not linked to backend
    
    GUI refresh button now works with backend
    
    GUI cleaning up code setting up for config window

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@ import sys
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt
 from main import Main
+
 class MainWindow(QMainWindow):
 
     def __init__(self):
@@ -10,70 +11,70 @@ class MainWindow(QMainWindow):
         self.setGeometry(500,500,500,200)
         self.setWindowTitle("Desktop Refresh")
 
-
-        self.Layout = QVBoxLayout()
-
-        TitleLabel = QLabel("Desktop Refresh")
-        TitleLabel.setObjectName('TitleHeader')
-        self.Layout.addWidget(TitleLabel)
-        TitleLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-
-        self.ButtonEvents = self.ButtonEvents()
-        self.DisplayButtons()
-
-        self.SetStyleSheet()
-
-    def SetStyleSheet(self):
-        with open('StyleSheet.qss', 'r') as File:
-            Styles = File.read()
-            self.setStyleSheet(Styles)
+        self.setCentralWidget(self.BaseMenu())
 
 
-
-    def DisplayButtons(self):
-
-        RefreshButton = QPushButton("Refresh", self)
-        RefreshButton.setObjectName('RefreshButton')
-        RefreshButton.clicked.connect(self.ButtonHandler)
-        #RefreshButton.setGeometry(250,250,100,50)
-        self.Layout.addWidget(RefreshButton)
-
-
-        ConfigButton = QPushButton("Config", self)
-        ConfigButton.setObjectName('ConfigButton')
-        ConfigButton.clicked.connect(self.ButtonHandler)
-        #ConfigButton.setGeometry(250,350,100,50)
-        self.Layout.addWidget(ConfigButton)
-
-        ButtonWidget = QWidget()
-        ButtonWidget.setLayout(self.Layout)
-        self.setCentralWidget(ButtonWidget)
-
-
-    def ButtonHandler(self):
-        sender = self.sender()  # Get the sender object
-        ButtonName = sender.objectName()  # Get the object name of the sender
-        print(f"Button clicked: {ButtonName}")
-
-        if ButtonName == "RefreshButton":
-            print("Refresh Button Handler Flag")
-            self.ButtonEvents.RefreshButtonEvent()
-
-        if ButtonName == "ConfigButton":
-            print("Config Button Handler Flag")
-            self.ButtonEvents.ConfigButtonEvent()
-
-
-    class ButtonEvents:
+    class BaseMenu(QWidget):
         def __init__(self):
-            super(MainWindow).__init__()
+            super().__init__()
 
-        def RefreshButtonEvent(self):
-            Main.Start()
-            print("Refresh Button Event")
+            self.ButtonEvents = self.ButtonEvents()
 
-        def ConfigButtonEvent(self):
-            print("Config Button Event")
+            self.layout = QVBoxLayout()
+            self.setLayout(self.layout)
+
+            TitleLabel = QLabel("Desktop Refresh")
+            TitleLabel.setObjectName('TitleHeader')
+            self.layout.addWidget(TitleLabel)
+            TitleLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+            self.SetStyleSheet()
+            self.DisplayButtons()
+
+        def SetStyleSheet(self):
+            with open('StyleSheet.qss', 'r') as File:
+                Styles = File.read()
+                self.setStyleSheet(Styles)
+
+
+
+        def DisplayButtons(self):
+
+            RefreshButton = QPushButton("Refresh", self)
+            RefreshButton.setObjectName('RefreshButton')
+            RefreshButton.clicked.connect(self.ButtonHandler)
+            self.layout.addWidget(RefreshButton)
+
+
+            ConfigButton = QPushButton("Config", self)
+            ConfigButton.setObjectName('ConfigButton')
+            ConfigButton.clicked.connect(self.ButtonHandler)
+            self.layout.addWidget(ConfigButton)
+
+        def ButtonHandler(self):
+            sender = self.sender()  # Get the sender object
+            ButtonName = sender.objectName()  # Get the object name of the sender
+            print(f"Button clicked: {ButtonName}")
+
+            if ButtonName == "RefreshButton":
+                print("Refresh Button Handler Flag")
+                self.ButtonEvents.RefreshButtonEvent()
+
+            if ButtonName == "ConfigButton":
+                print("Config Button Handler Flag")
+                self.ButtonEvents.ConfigButtonEvent()
+
+
+        class ButtonEvents:
+            def __init__(self):
+                super(MainWindow).__init__()
+
+            def RefreshButtonEvent(self):
+                Main.Start()
+                print("Refresh Button Event")
+
+            def ConfigButtonEvent(self):
+                print("Config Button Event")
 
 
 app = QApplication(sys.argv)
